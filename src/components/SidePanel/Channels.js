a549db275f157b57159b890a5cd1e82b518e7a0d
Clark Perfecto <a1clark1a@gmail.com>
2020-06-24 18:15:15 -0700

    Set First Channel on page load and added highlights when active channel is clicked or if its first channel

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -13,6 +13,8 @@ class Channels extends Component {
     channelDetails: "",
     channelsRef: firebase.database().ref("channels"),
     modal: false,
+    firstLoad: true,
+    activeChannel: "",
   }
 
   componentDidMount() {
@@ -23,8 +25,18 @@ class Channels extends Component {
     let loadedChannels = []
     this.state.channelsRef.on("child_added", (snap) => {
       loadedChannels.push(snap.val())
-      this.setState({ channels: loadedChannels })
+      this.setState({ channels: loadedChannels }, () => this.setFirstChannel())
     })
+  }
+
+  setFirstChannel = () => {
+    let firstChannel
+    if (this.state.firstLoad && this.state.channels.length > 0) {
+      firstChannel = this.state.channels[0]
+      this.props.setCurrentChannel(firstChannel)
+      this.setActiveChannel(firstChannel)
+    }
+    this.setState({ firstLoad: false })
   }
 
   addChannel = () => {
@@ -85,13 +97,19 @@ class Channels extends Component {
         onClick={() => this.changeChannel(channel)}
         name={channel.name}
         style={{ opacity: 0.7 }}
+        active={channel.id === this.state.activeChannel}
       >
         # {channel.name}
       </Menu.Item>
     ))
 
   changeChannel = (channel) => {
+    this.setActiveChannel(channel)
     this.props.setCurrentChannel(channel)
+  }
+
+  setActiveChannel = (channel) => {
+    this.setState({ activeChannel: channel.id })
   }
 
   render() {
